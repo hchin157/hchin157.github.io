5529f624aca752ae336543d8509f71eca11fabb2
hchin <hchin2@hofstra.pride.edu>
2020-07-08 18:54:54 -0400

    Changed some names, moved scripting to JS File.
    Added
    -function that adds species
    -function that adds ranks
    -onload
    -req JSON

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var alldata = require('./data.json')
+const full_list = alldata.full_species_list
+
 let entry_template = `
   <h3></h3>
   <p></p>
@@ -12,21 +15,43 @@ form.addEventListener('submit', (event) => {
 })
 
 let adder = function() {
-  const input = document.querySelector('[id=species]');
+  const input = document.querySelector('#species');
 
   let entry = document.createElement("div");
-  entry.setAttribute("class", "companion");
+  entry.setAttribute("class", "entry");
   entry.innerHTML = entry_template;
 
-  entry.querySelector("h3").innerText = input.value;
-  entry.querySelector("p").innerText = "";
+  entry.querySelector("h3").innerText = input.selectedIndex;
+  entry.querySelector("p").innerText = input.value;
   entry.querySelector("img").src = "images/sprites_full/"+ input.value + ".gif";
   entry.querySelector("img").setAttribute("class", "portrait");
 
   entry_list.appendChild(entry);
 }
 
-
-let loadDex = function() {
+function loadDex(list) {
   console.log("Loaded!");
+
+  list.forEach((pokemon) => {
+    let species = document.querySelector("[id='species']")
+    let option = document.createElement("option")
+    option.text = pokemon.name
+    species.add(option)
+  });
 }
+
+function loadRank(){
+  console.log("Also loaded!");
+  let ranker = document.querySelector("[id='rank']")
+  var i;
+  for (i = 0; i < 10; i++) {
+    let rank = document.createElement("option")
+    rank.text = i+1
+    ranker.add(rank)
+  }
+}
+
+window.addEventListener("load", (event) => {
+  loadDex(full_list);
+  loadRank();
+  });
