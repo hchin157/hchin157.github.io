4224b73f7503345d72f5aba3e93528d4e480ac83
hchin <hchin2@hofstra.pride.edu>
2020-07-28 21:51:07 -0400

    Gender Update part 1
    Can now
    - add gender to dex entries, based on species
    - gender select will only have the available genders for that species
     (the options from the last submit are removed)
    
    Still some bugs to be fixed.

--- bundle.js
+++ bundle.js
@@ -257,7 +257,7 @@ const full_list = alldata.full_species_list
 
 /* Add entries to the "dex" div element.*/
 let entry_template = `
-  <h3></h3> <h4></h4>
+  <h3></h3> <h4></h4> <h5></h5>
   <p></p>
   <img></img>
 `
@@ -265,37 +265,99 @@ let entry_list = document.querySelector('.dex');
 
 const select = document.getElementById('species')
 const ranker = document.getElementById('rank')
+const gender = document.getElementById('gender')
 const submit = document.querySelector("[type='submit']")
 const form = document.querySelector(".annex");
 
 select.addEventListener('click', (event) => {
-  checker(select, submit);
+  checker(select, ranker, gender, submit);
 })
 
-let checker = function(input, submit) { //checks if a species is selected before allowing submission.
+let checker = function(input, input2, input3, submit) { //checks if a species is selected before allowing submission.
   const indexer = input.selectedIndex
   if (indexer != 0) {
-    submit.disabled = false
+    const x = document.querySelectorAll('.hidden'); //selects the hidden labels and select elements.
+    x.forEach((item) => { item.style.display = "inline"; } ) //makes them visible.
+    submit.disabled = false;
+
+    //updated loadRank()
+    loadGender(input, input3) //loads appropriate genders.
   }
+}
+
+let loadGender = function(z, y) { //species, and gender select
+  let crude = z.value
+  let species = comparator(z.selectedIndex, crude).substr(4) //just want to trim the bit before, so comparator() is used.
+  console.log(species)
+  let genders = ["Male", "Female", "Genderless",]
+  let final = []
+  switch(species) {
+    case 'Mothim':
+    case 'Gallade':
+      final.push(genders[0])
+      break; //male only
+    case 'Happiny':
+    case 'Chansey':
+    case 'Blissey':
+    case 'Wormadam':
+    case 'Vespiquen':
+    case 'Froslass':
+    case 'Cresselia':
+      final.push(genders[1])
+      break; //female only
+    case 'Bronzor':
+    case 'Bronzong':
+    case 'Rotom':
+    case 'Magnemite':
+    case 'Magneton':
+    case 'Magnezone':
+    case 'Porygon':
+    case 'Porygon2':
+    case 'Porygon-Z':
+    case 'Uxie':
+    case 'Mesprit':
+    case 'Azelf':
+    case 'Dialga':
+    case 'Palkia':
+    case 'Giratina':
+    case 'Heatran':
+    case 'Regigigas':
+    case 'Phione':
+    case 'Manaphy':
+    case 'Darkrai':
+    case 'Shaymin':
+    case 'Arceus':
+      final.push(genders[2])
+      break; //genderless
+    default:
+      final.push(genders[0])
+      final.push(genders[1])
+  }
+  final.forEach((item) => {
+    let option = document.createElement("option")
+    option.text = item
+    y.add(option)
+  });
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (party.length < 4) { //max party of 4.
-    adder(select, ranker);
+    adder(select, ranker, gender);
   } else {
     alert("You've reached the maximum capacity of your party.\nPlease use the PC.")
   }
   resetter(select, submit);
 })
 
-let adder = function(input, input2) { //adds a Pokemon to the dex, and the party array.
+let adder = function(input, input2, input3) { //adds a Pokemon to the dex, and the party array.
   let entry = document.createElement("div");
   entry.setAttribute("class", "entry");
   entry.innerHTML = entry_template;
 
-  entry.querySelector("h3").innerText = "#"+input.selectedIndex;
-  entry.querySelector("h4").innerText = "Rank: "+input2.value;
+  entry.querySelector("h3").innerText = "#"+input.selectedIndex; //the pokedex number.
+  entry.querySelector("h4").innerText = "Rank: "+input2.value; //the rank.
+  entry.querySelector("h5").innerText = "Gender: "+ input3.value; //gender.
   entry.querySelector("p").innerText = input.value;
   entry.querySelector("img").src = "images/sprites_full/" + comparator(input.selectedIndex, input.value) + ".gif";
   entry.querySelector("img").setAttribute("class", "portrait");
@@ -304,6 +366,7 @@ let adder = function(input, input2) { //adds a Pokemon to the dex, and the party
 
   partyAdd(party, input.value, input2.value);
 }
+
 let comparator = function (index, value) {
   let first = "";
   let second = "";
@@ -325,6 +388,14 @@ let resetter = function(input, submit) { //resets the form to onload state.
   input[0].selected = true
   submit.disabled = true
   document.getElementById("rank")[0].selected = true //selects rank 1 on reset.
+
+  let i, j = document.getElementById("gender").options.length - 1;
+     for(i = j; i >= 0; i--) {
+        document.getElementById("gender").remove(i);
+     } //removes the options that were added.
+
+  const x = document.querySelectorAll('.hidden'); //selects the visible labels and select elements.
+  x.forEach((item) => { item.style.display = "none"; } ) //makes them hidden.
 }
 
 
